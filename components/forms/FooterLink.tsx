6a76472e1bda383c6aaf1e3be1345cec19b8f6c8
w00ye0l <dnduf158@gmail.com>
2025-10-02 00:20:36 +0900

    feat: implement Auth UI & Layout

---
+++ components/forms/FooterLink.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function FooterLink({ text, linkText, href }: FooterLinkProps) {
+  return (
+    <div className="text-center pt-4">
+      <p className="text-sm text-gray-500">
+        {text}
+        {` `}
+        <Link href={href} className="footer-link">
+          {linkText}
+        </Link>
+      </p>
+    </div>
+  );
+}
